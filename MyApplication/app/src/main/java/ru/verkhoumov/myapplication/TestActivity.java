87aaa0abbb0180e52aa4bdda95eae07e74e3c4d7
verkhoumov <verkhoumov@gmail.com>
2018-01-08 18:45:25 +0300

    MyApplication

---
+++ MyApplication/app/src/main/java/ru/verkhoumov/myapplication/TestActivity.java
@@ -0,0 +1,42 @@
+package ru.verkhoumov.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class TestActivity extends AppCompatActivity {
+    private Button mYesButton;
+    private Button mNoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        /**
+         * Привязываем кнопки к элементам приложения и вешаем
+         * событие нажатия на каждую из них.
+         */
+        mYesButton = (Button) findViewById(R.id.yes_button);
+        mYesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast
+                        .makeText(TestActivity.this, R.string.correct_answer, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        mNoButton = (Button) findViewById(R.id.no_button);
+        mNoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Toast
+                        .makeText(TestActivity.this, R.string.incorrect_answer, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+}
