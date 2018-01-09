734164f31f2f40094752a7bad53dab7543695b76
verkhoumov <verkhoumov@gmail.com>
2018-01-10 02:50:17 +0300

    Самостоятельная работа

---
+++ Layouts/app/src/main/java/ru/verkhoumov/layouts/TimerWatch.java
@@ -0,0 +1,13 @@
+package ru.verkhoumov.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerWatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_watch);
+    }
+}
