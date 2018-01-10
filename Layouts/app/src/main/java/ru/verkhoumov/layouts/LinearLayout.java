11cdc4db2ded093d7b6a6620be73fc58142421b1
verkhoumov <verkhoumov@gmail.com>
2018-01-10 16:48:18 +0300

    Layouts addons

--- Layouts/app/src/main/java/ru/verkhoumov/layouts/LinearLayout.java
+++ Layouts/app/src/main/java/ru/verkhoumov/layouts/LinearLayout.java
@@ -8,6 +8,6 @@ public class LinearLayout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cat);
+        setContentView(R.layout.activity_linear_layout);
     }
 }
